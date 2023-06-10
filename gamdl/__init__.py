caed322fd08e3cc72155fc9b97481e00113f8024
R. M <50295204+glomatico@users.noreply.github.com>
2023-06-10 12:09:33 -0300

    Update __init__.py

--- gamdl/__init__.py
+++ gamdl/__init__.py
@@ -1,108 +1,109 @@
-import shutil
 import argparse
+import shutil
 import traceback
+
 from .gamdl import Gamdl
 
-__version__ = '1.9.1'
+__version__ = "1.9.1"
 
 
 def main():
-    if not shutil.which('mp4decrypt'):
-        raise Exception('mp4decrypt is not on PATH')
-    if not shutil.which('MP4Box'):
-        raise Exception('MP4Box is not on PATH')
+    if not shutil.which("mp4decrypt"):
+        raise Exception("mp4decrypt is not on PATH")
+    if not shutil.which("MP4Box"):
+        raise Exception("MP4Box is not on PATH")
     parser = argparse.ArgumentParser(
-        description='Download Apple Music songs/music videos/albums/playlists',
+        description="Download Apple Music songs/music videos/albums/playlists",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
-        'url',
-        help='Apple Music song/music video/album/playlist URL(s)',
-        nargs='*',
+        "url",
+        help="Apple Music song/music video/album/playlist URL(s)",
+        nargs="*",
     )
     parser.add_argument(
-        '-u',
-        '--urls-txt',
-        help='Read URLs from a text file',
-        nargs='?',
+        "-u",
+        "--urls-txt",
+        help="Read URLs from a text file",
+        nargs="?",
     )
     parser.add_argument(
-        '-w',
-        '--wvd-location',
-        default='./*.wvd',
-        help='.wvd file location (ignored if using -l/--lrc-only)',
+        "-w",
+        "--wvd-location",
+        default="./*.wvd",
+        help=".wvd file location (ignored if using -l/--lrc-only)",
     )
     parser.add_argument(
-        '-f',
-        '--final-path',
-        default='./Apple Music',
-        help='Final Path',
+        "-f",
+        "--final-path",
+        default="./Apple Music",
+        help="Final Path",
     )
     parser.add_argument(
-        '-t',
-        '--temp-path',
-        default='./temp',
-        help='Temp Path',
+        "-t",
+        "--temp-path",
+        default="./temp",
+        help="Temp Path",
     )
     parser.add_argument(
-        '-c',
-        '--cookies-location',
-        default='./cookies.txt',
-        help='Cookies location',
+        "-c",
+        "--cookies-location",
+        default="./cookies.txt",
+        help="Cookies location",
     )
     parser.add_argument(
-        '-m',
-        '--disable-music-video-skip',
-        action='store_true',
-        help='Disable music video skip on playlists/albums',
+        "-m",
+        "--disable-music-video-skip",
+        action="store_true",
+        help="Disable music video skip on playlists/albums",
     )
     parser.add_argument(
-        '-p',
-        '--prefer-hevc',
-        action='store_true',
-        help='Prefer HEVC over AVC',
+        "-p",
+        "--prefer-hevc",
+        action="store_true",
+        help="Prefer HEVC over AVC",
     )
     parser.add_argument(
-        '-o',
-        '--overwrite',
-        action='store_true',
-        help='Overwrite existing files',
+        "-o",
+        "--overwrite",
+        action="store_true",
+        help="Overwrite existing files",
     )
     parser.add_argument(
-        '-n',
-        '--no-lrc',
-        action='store_true',
+        "-n",
+        "--no-lrc",
+        action="store_true",
         help="Don't create .lrc file (ignored if using -l/--lrc-only)",
     )
     parser.add_argument(
-        '-l',
-        '--lrc-only',
-        action='store_true',
-        help='Skip downloading songs and only create .lrc files'
+        "-l",
+        "--lrc-only",
+        action="store_true",
+        help="Skip downloading songs and only create .lrc files",
     )
     parser.add_argument(
-        '-s',
-        '--skip-cleanup',
-        action='store_true',
-        help='Skip cleanup',
+        "-s",
+        "--skip-cleanup",
+        action="store_true",
+        help="Skip cleanup",
     )
     parser.add_argument(
-        '-e',
-        '--print-exceptions',
-        action='store_true',
-        help='Print execeptions',
+        "-e",
+        "--print-exceptions",
+        action="store_true",
+        help="Print execeptions",
     )
     parser.add_argument(
-        '-v',
-        '--version',
-        action='version',
-        version=f'%(prog)s {__version__}',
+        "-v",
+        "--version",
+        action="version",
+        version=f"%(prog)s {__version__}",
     )
     args = parser.parse_args()
     if not args.url and not args.urls_txt:
-        parser.error('you must specify an url or a text file using -u/--urls-txt')
+        parser.error("you must specify an url or a text file using -u/--urls-txt")
     if args.urls_txt:
-        with open(args.urls_txt, 'r', encoding='utf8') as f:
+        with open(args.urls_txt, "r", encoding="utf8") as f:
             args.url = f.read().splitlines()
     dl = Gamdl(
         args.wvd_location,
@@ -124,39 +125,61 @@ def main():
             exit(1)
         except:
             error_count += 1
-            print(f'Failed to check URL {i + 1}/{len(args.url)}')
+            print(f"Failed to check URL {i + 1}/{len(args.url)}")
             if args.print_exceptions:
                 traceback.print_exc()
     for i, url in enumerate(download_queue):
         for j, track in enumerate(url):
-            print(f'Downloading "{track["attributes"]["name"]}" (track {j + 1}/{len(url)} from URL {i + 1}/{len(download_queue)})')
-            track_id = track['id']
+            print(
+                f'Downloading "{track["attributes"]["name"]}" (track {j + 1}/{len(url)} from URL {i + 1}/{len(download_queue)})'
+            )
+            track_id = track["id"]
             try:
                 webplayback = dl.get_webplayback(track_id)
-                if track['type'] == 'music-videos':
-                    tags = dl.get_tags_music_video(track['attributes']['url'].split('/')[-1].split('?')[0])
-                    final_location = dl.get_final_location('.m4v', tags)
+                if track["type"] == "music-videos":
+                    tags = dl.get_tags_music_video(
+                        track["attributes"]["url"].split("/")[-1].split("?")[0]
+                    )
+                    final_location = dl.get_final_location(".m4v", tags)
                     if final_location.exists() and not args.overwrite:
                         continue
-                    stream_url_video, stream_url_audio = dl.get_stream_url_music_video(webplayback)
-                    decryption_keys_audio = dl.get_decryption_keys_music_video(stream_url_audio, track_id)
+                    stream_url_video, stream_url_audio = dl.get_stream_url_music_video(
+                        webplayback
+                    )
+                    decryption_keys_audio = dl.get_decryption_keys_music_video(
+                        stream_url_audio, track_id
+                    )
                     encrypted_location_audio = dl.get_encrypted_location_audio(track_id)
                     dl.download(encrypted_location_audio, stream_url_audio)
                     decrypted_location_audio = dl.get_decrypted_location_audio(track_id)
-                    dl.decrypt(encrypted_location_audio, decrypted_location_audio, decryption_keys_audio)
-                    decryption_keys_video = dl.get_decryption_keys_music_video(stream_url_video, track_id)
+                    dl.decrypt(
+                        encrypted_location_audio,
+                        decrypted_location_audio,
+                        decryption_keys_audio,
+                    )
+                    decryption_keys_video = dl.get_decryption_keys_music_video(
+                        stream_url_video, track_id
+                    )
                     encrypted_location_video = dl.get_encrypted_location_video(track_id)
                     dl.download(encrypted_location_video, stream_url_video)
                     decrypted_location_video = dl.get_decrypted_location_video(track_id)
-                    dl.decrypt(encrypted_location_video, decrypted_location_video, decryption_keys_video)
-                    fixed_location = dl.get_fixed_location(track_id, '.m4v')
-                    dl.fixup_music_video(decrypted_location_audio, decrypted_location_video, fixed_location)
+                    dl.decrypt(
+                        encrypted_location_video,
+                        decrypted_location_video,
+                        decryption_keys_video,
+                    )
+                    fixed_location = dl.get_fixed_location(track_id, ".m4v")
+                    dl.fixup_music_video(
+                        decrypted_location_audio,
+                        decrypted_location_video,
+                        fixed_location,
+                    )
                     final_location.parent.mkdir(parents=True, exist_ok=True)
                     dl.move_final(final_location, fixed_location, tags)
                 else:
                     unsynced_lyrics, synced_lyrics = dl.get_lyrics(track_id)
                     tags = dl.get_tags_song(webplayback, unsynced_lyrics)
-                    final_location = dl.get_final_location('.m4a', tags)
+                    final_location = dl.get_final_location(".m4a", tags)
                     if args.lrc_only:
                         final_location.parent.mkdir(parents=True, exist_ok=True)
                         dl.make_lrc(final_location, synced_lyrics)
@@ -169,7 +192,7 @@ def main():
                     dl.download(encrypted_location, stream_url)
                     decrypted_location = dl.get_decrypted_location_audio(track_id)
                     dl.decrypt(encrypted_location, decrypted_location, decryption_keys)
-                    fixed_location = dl.get_fixed_location(track_id, '.m4a')
+                    fixed_location = dl.get_fixed_location(track_id, ".m4a")
                     dl.fixup_song(decrypted_location, fixed_location)
                     final_location.parent.mkdir(parents=True, exist_ok=True)
                     dl.move_final(final_location, fixed_location, tags)
@@ -179,8 +202,10 @@ def main():
                 exit(1)
             except:
                 error_count += 1
-                print(f'Failed to download "{track["attributes"]["name"]}" (track {j + 1}/{len(url)} from URL {i + 1}/{len(download_queue)})')
+                print(
+                    f'Failed to download "{track["attributes"]["name"]}" (track {j + 1}/{len(url)} from URL {i + 1}/{len(download_queue)})'
+                )
                 if args.print_exceptions:
                     traceback.print_exc()
             dl.cleanup()
-    print(f'Done ({error_count} error(s))')
+    print(f"Done ({error_count} error(s))")

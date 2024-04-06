f28b75b66dc957935421a35cff2a082b742ce127
alacleaker <50295204+glomatico@users.noreply.github.com>
2024-04-06 19:09:38 -0300

    add more formats

--- gamdl/enums.py
+++ gamdl/enums.py
@@ -9,8 +9,12 @@ class DownloadMode(Enums):
 class SongCodec(Enums):
     AAC_LEGACY = "aac-legacy"
     AAC_HE_LEGACY = "aac-he-legacy"
+    AAC = "aac"
+    AAC_HE = "aac-he"
     AAC_BINAURAL = "aac-binaural"
     AAC_DOWNMIX = "aac-downmix"
+    AAC_HE_BINAURAL = "aac-he-binaural"
+    AAC_HE_DOWNMIX = "aac-he-downmix"
     ALAC = "alac"
     ATMOS = "atmos"
 

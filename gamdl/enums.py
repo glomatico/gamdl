57494e8e0a828e031365f59b3b79afd92ee0478b
alacleaker <50295204+glomatico@users.noreply.github.com>
2024-04-06 17:59:52 -0300

    Create enums.py

---
+++ gamdl/enums.py
@@ -0,0 +1,20 @@
+from enums import Enums
+
+
+class DownloadMode(Enums):
+    YTDLP = "ytdlp"
+    NM3U8DLRE = "nm3u8dlre"
+
+
+class SongCodec(Enums):
+    AAC_LEGACY = "aac-legacy"
+    AAC_HE_LEGACY = "aac-he-legacy"
+    AAC_BINAURAL = "aac-binaural"
+    AAC_DOWNMIX = "aac-downmix"
+    ALAC = "alac"
+    ATMOS = "atmos"
+
+
+class ArtworkFormat(Enums):
+    JPEG = "jpeg"
+    PNG = "png"

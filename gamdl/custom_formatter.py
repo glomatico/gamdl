4fb1fb609b07547e15ae48bcdcf93821d979d042
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-02-23 16:28:38 -0300

    Update custom_formatter.py

--- gamdl/custom_formatter.py
+++ gamdl/custom_formatter.py
@@ -6,18 +6,19 @@ from .utils import color_text
 
 
 class CustomFormatter(logging.Formatter):
-    basic_format = "[%(levelname)-8s %(asctime)s]"
-    formats = {
-        logging.DEBUG: color_text(basic_format, colorama.Style.DIM),
-        logging.INFO: color_text(basic_format, colorama.Fore.GREEN),
-        logging.WARNING: color_text(basic_format, colorama.Fore.YELLOW),
-        logging.ERROR: color_text(basic_format, colorama.Fore.RED),
-        logging.CRITICAL: color_text(basic_format, colorama.Fore.RED),
+    base_format = "[%(levelname)-8s %(asctime)s]"
+    format_colors = {
+        logging.DEBUG: colorama.Style.DIM,
+        logging.INFO: colorama.Fore.GREEN,
+        logging.WARNING: colorama.Fore.YELLOW,
+        logging.ERROR: colorama.Fore.RED,
+        logging.CRITICAL: colorama.Fore.RED,
     }
     date_format = "%H:%M:%S"
 
     def format(self, record: logging.LogRecord) -> str:
         return logging.Formatter(
-            self.formats.get(record.levelno) + " %(message)s",
+            color_text(self.base_format, self.format_colors.get(record.levelno))
+            + " %(message)s",
             datefmt=self.date_format,
         ).format(record)

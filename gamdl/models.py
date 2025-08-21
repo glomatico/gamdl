74a28933a2597d79439b3bf09689157ab8b9c8a3
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-08-21 14:01:00 -0300

    Refactor MediaTags ID types and MP4 tag conversion

--- gamdl/models.py
+++ gamdl/models.py
@@ -46,15 +46,15 @@ class StreamInfoAv:
 class MediaTags:
     album: str = None
     album_artist: str = None
-    album_id: str = None
+    album_id: int = None
     album_sort: str = None
     artist: str = None
-    artist_id: str = None
+    artist_id: int = None
     artist_sort: str = None
     comment: str = None
     compilation: bool = None
     composer: str = None
-    composer_id: str = None
+    composer_id: int = None
     composer_sort: str = None
     copyright: str = None
     date: str = None
@@ -62,13 +62,13 @@ class MediaTags:
     disc_total: int = None
     gapless: bool = None
     genre: str = None
-    genre_id: str = None
+    genre_id: int = None
     lyrics: str = None
     media_type: MediaType = None
     rating: MediaRating = None
     storefront: str = None
     title: str = None
-    title_id: str = None
+    title_id: int = None
     title_sort: str = None
     track: int = None
     track_total: int = None
@@ -76,20 +76,24 @@ class MediaTags:
 
     def to_mp4_tags(self) -> dict[str, typing.Any]:
         disc_mp4 = [
-            self.disc if self.disc is not None else 0,
-            self.disc_total if self.disc is not None else 0,
+            [
+                self.disc if self.disc is not None else 0,
+                self.disc_total if self.disc is not None else 0,
+            ]
         ]
-        if disc_mp4[0] == 0 and disc_mp4[1] == 0:
+        if disc_mp4[0][0] == 0 and disc_mp4[0][1] == 0:
             disc_mp4 = [None]
 
         track_mp4 = [
-            self.track if self.track is not None else 0,
-            self.track_total if self.track is not None else 0,
+            [
+                self.track if self.track is not None else 0,
+                self.track_total if self.track is not None else 0,
+            ]
         ]
-        if track_mp4[0] == 0 and track_mp4[1] == 0:
+        if track_mp4[0][0] == 0 and track_mp4[0][1] == 0:
             track_mp4 = [None]
 
-        return {
+        mp4_tags = {
             "\xa9alb": [self.album],
             "aART": [self.album_artist],
             "plID": [self.album_id],
@@ -98,24 +102,23 @@ class MediaTags:
             "atID": [self.artist_id],
             "soar": [self.artist_sort],
             "\xa9cmt": [self.comment],
-            "cpil": [int(self.compilation)],
+            "cpil": [int(self.compilation) if self.compilation is not None else None],
             "\xa9wrt": [self.composer],
             "cmID": [self.composer_id],
             "soco": [self.composer_sort],
             "cprt": [self.copyright],
             "\xa9day": [self.date],
             "disk": disc_mp4,
-            "pgap": [int(self.gapless)],
+            "pgap": [bool(self.gapless) if self.gapless is not None else None],
             "\xa9gen": [self.genre],
             "\xa9lyr": [self.lyrics],
             "geID": [self.genre_id],
-            "stik": (
-                [int(self.media_type)] if int(self.media_type) is not None else [None]
-            ),
-            "rtng": [int(self.rating)],
+            "stik": [int(self.media_type) if self.media_type is not None else None],
+            "rtng": [int(self.rating) if self.rating is not None else None],
             "sfID": [self.storefront],
             "\xa9nam": [self.title],
             "cnID": [self.title_id],
             "sonm": [self.title_sort],
             "xid ": [self.xid],
         }
+        return {k: v for k, v in mp4_tags.items() if v[0] is not None}

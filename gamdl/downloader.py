0b43049dc87327f5eedd8ee977f66a8578c0a52a
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-08-22 14:38:54 -0300

    Fallback to media ID if catalogId is missing

--- gamdl/downloader.py
+++ gamdl/downloader.py
@@ -293,7 +293,7 @@ class Downloader:
         library_media_metadata: dict,
     ) -> str | None:
         play_params = library_media_metadata["attributes"].get("playParams", {})
-        return play_params.get("catalogId")
+        return play_params.get("catalogId", library_media_metadata["id"])
 
     def is_media_streamable(
         self,

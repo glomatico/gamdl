9f93b0e791ae1701fd34c6d4459bbaac690fa342
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-08-31 13:55:59 -0300

    Refactor exception classes for clarity and consistency

--- gamdl/exceptions.py
+++ gamdl/exceptions.py
@@ -1,13 +1,24 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+
 class MediaNotStreamableException(Exception):
-    def __init__(self, message: str = "Media is not streamable"):
-        super().__init__(message)
+    DEFAULT_MESSAGE = "Media is not streamable"
+
+    def __init__(self):
+        super().__init__(self.DEFAULT_MESSAGE)
 
 
 class MediaFileAlreadyExistsException(Exception):
-    def __init__(self, message: str = "Media file already exists"):
-        super().__init__(message)
+    DEFAULT_MESSAGE = "Media file already exists at '{media_path}'"
+
+    def __init__(self, media_path: Path):
+        super().__init__(self.DEFAULT_MESSAGE.format(media_path=media_path))
 
 
 class MediaFormatNotAvailableException(Exception):
-    def __init__(self, message: str = "Requested media format or codec not available"):
-        super().__init__(message)
+    DEFAULT_MESSAGE = "Requested media format or codec not available"
+
+    def __init__(self):
+        super().__init__(self.DEFAULT_MESSAGE)

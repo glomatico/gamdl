8335af0f7920f5840d4ce80c52fa53390ed34dc3
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2026-04-13 22:25:48 -0300

    Refactor API exception classes

--- gamdl/api/exceptions.py
+++ gamdl/api/exceptions.py
@@ -1,7 +1,25 @@
 from ..utils import GamdlError
 
 
-class ApiError(GamdlError):
-    def __init__(self, message: str, status_code: int):
-        super().__init__(f"API Error {status_code}: {message}")
+class GamdlApiError(GamdlError):
+    pass
+
+
+class GamdlApiResponseError(GamdlApiError):
+    def __init__(
+        self,
+        message: str,
+        content: str,
+        status_code: int | None = None,
+    ):
+        self.message = message
+        self.content = content
         self.status_code = status_code
+
+        if status_code is not None:
+            message = f"{message} (Status code: {status_code})"
+
+        if content:
+            message += f": {content}"
+
+        super().__init__(message)
